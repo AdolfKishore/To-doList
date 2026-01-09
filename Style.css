62ab697ba35594cbc0adb8dd4b2c9c5e19fefd83
Kishore Raj YR <132810380+AdolfKishore@users.noreply.github.com>
2026-01-09 14:57:49 +0530

    Create Style.css

---
+++ Style.css
@@ -0,0 +1,31 @@
+body {
+  font-family: Arial, sans-serif;
+  background: #f4f4f4;
+}
+
+.container {
+  width: 300px;
+  margin: 50px auto;
+}
+
+ul {
+  padding: 0;
+}
+
+li {
+  list-style: none;
+  padding: 8px;
+  background: #fff;
+  margin-top: 5px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.completed {
+  text-decoration: line-through;
+  color: gray;
+}
+
+button {
+  cursor: pointer;
+}
